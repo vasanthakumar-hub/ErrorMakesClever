ef4620b4f9dee27cdc8915d2bcb5df0e22d75765
vasanthakumar-hub <vasanthakumar0969@gmail.com>
2025-03-19 09:57:54 +0530

    javascript basics

---
+++ emc52-ifelse.js
@@ -0,0 +1,16 @@
+var score=71.8
+
+if(score<=50)
+{
+    console.log("You Need to Improve")
+}
+
+else if(score>50 && score<=70)
+{
+    console.log("Good Job")
+}
+
+else if(score>70)
+{
+    console.log("Excellent Performance")
+}
