64d52109a1005a2f87da7500630f95fc81298478
vasanthakumar-hub <vasanthakumar0969@gmail.com>
2025-03-21 08:09:05 +0530

    random numbers

---
+++ emc63-random.js
@@ -0,0 +1,12 @@
+var a = Math.random(); // generates ransom number betweem 0.0 to 0.9
+
+console.log(a)
+console.log(a*10)
+
+var b = Math.floor(6.25) //converts this value to 6 by decreasing .25. 
+
+console.log(b)
+
+var c = Math.random();
+
+console.log(Math.floor(c*10)+1) // +1 is added to generate random numbers from 1 to 10.
