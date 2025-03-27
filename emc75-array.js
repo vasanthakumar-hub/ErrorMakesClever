b152448007a03c29c8cdccd31d47774d047ed335
vasanthakumar-hub <vasanthakumar0969@gmail.com>
2025-03-27 10:53:40 +0530

    todo list basics

---
+++ emc75-array.js
@@ -0,0 +1,13 @@
+var fruit = ["Apple","Orange","Grape","Banana"] //array is a group of data
+//console.log(fruit[0])
+//array always starts from zero
+
+console.log(fruit.length) // 4
+
+for(count=0;count<fruit.length;count=count+1)
+{
+    //console.log(count) 
+    console.log(fruit[count])
+}
+
+//for loop is used to get a group of data
